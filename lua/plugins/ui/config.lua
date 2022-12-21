904429505132d365a8a7f14a726f4ab9438ad375
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2022-12-22 01:09:56 +0530

    feat: added a bunch of themes

---
+++ lua/plugins/ui/config.lua
@@ -0,0 +1,29 @@
+return {
+  theme = {
+    name = 'catpuccin',
+    transparent = false,
+    hide_eob = true,
+    comments = { 'italic' },
+    functions = { 'italic' },
+    keywords = { 'italic' },
+    strings = {},
+    variables = { 'italic' },
+    italics = {
+      comments = true,
+      functions = true,
+      keywords = true,
+      strings = false,
+      variables = true,
+    },
+    sidebars = {
+      'UltestSummary',
+      'terminal',
+      'packer',
+      'qf',
+      'neofs',
+      'nvim-tree',
+      'lir',
+      'JABS',
+    },
+  },
+}
--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -1,4 +1,34 @@
 local themes = {}
+local config = require 'plugins.ui.config'
+
+themes.blue_moon = {
+  plug = {
+    'kyazdani42/blue-moon',
+    config = function() require('plugins.ui.themes').blue_moon.setup() end,
+    lazy = false,
+  },
+  setup = function() vim.cmd.colorscheme 'blue-moon' end,
+}
+
+themes.calvera = {
+  plug = {
+    'yashguptaz/calvera-dark.nvim',
+    config = function() require('plugins.ui.themes').calvera.setup() end,
+    lazy = false,
+  },
+  setup = function()
+    vim.g.calvera_italic_comments = config.theme.italics.comments
+    vim.g.calvera_italic_keywords = config.theme.italics.keywords
+    vim.g.calvera_italic_functions = config.theme.italics.functions
+    vim.g.calvera_italic_variables = config.theme.italics.variables
+    vim.g.calvera_borders = true
+    vim.g.calvera_contrast = true
+    vim.g.calvera_hide_eob = true
+    vim.g.calvera_disable_background = config.theme.transparent
+    vim.g.transparent_bg = config.theme.transparent
+    require('calvera').set()
+  end,
+}
 
 themes.catppuccin = {
   plug = {
@@ -23,7 +53,101 @@ themes.catppuccin = {
   end,
 }
 
-themes.plug = themes.catppuccin.plug
-themes.setup = themes.catppuccin.setup
+themes.material = {
+  plug = {
+    'marko-cerovac/material.nvim',
+    config = function() require('plugins.ui.themes').material.setup() end,
+    lazy = false,
+  },
+  setup = function()
+    vim.g.material_style = 'deep ocean'
+    require('material').setup {
+      contrast = {
+        popup_menu = true,
+        sidebars = true,
+        floating_windows = true,
+        cursor_line = true,
+      },
+      italics = {
+        comments = true,
+        keywords = true,
+        functions = true,
+        variables = true,
+      },
+      contrast_filetypes = config.theme.sidebars,
+      disable = {
+        eob_lines = config.theme.hide_eob,
+        background = config.theme.transparent,
+      },
+    }
+    vim.cmd [[ colorscheme material ]]
+  end,
+}
+
+themes.rosepine = {
+  plug = {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+    config = function() require('plugins.ui.themes').rosepine.setup() end,
+    lazy = false,
+  },
+  setup = function()
+    vim.g.rose_pine_variant = 'base'
+    vim.g.rose_pine_enable_italics = true
+    vim.g.rose_pine_disable_background = config.theme.transparent
+    vim.cmd.colorscheme 'rose-pine'
+  end,
+}
+
+themes.sakura = {
+  plug = {
+    'numToStr/Sakura.nvim',
+    config = function() require('plugins.ui.themes').sakura.setup() end,
+    lazy = false,
+  },
+  setup = function()
+    local sakura = require 'sakura'
+    sakura.setup {
+      variant = 'moon',
+      transparent = config.theme.transparent,
+      italics = true,
+    }
+    sakura.load()
+  end,
+}
+
+themes.tokyonight = {
+  plug = {
+    'folke/tokyonight.nvim',
+    config = function() require('plugins.ui.themes').tokyonight.setup() end,
+    lazy = false,
+  },
+  setup = function()
+    require('tokyonight').setup {
+      style = 'night',
+      transparent = config.theme.transparent,
+      terminal_colors = true,
+      styles = {
+        comments = 'italic',
+        keywords = 'italic',
+        functions = 'italic',
+        variables = 'italic',
+        sidebars = 'dark',
+        floats = 'dark',
+      },
+      sidebars = config.theme.sidebars,
+      day_brightness = 0.3,
+      hide_inactive_statusline = false,
+      dim_inactive = false,
+      lualine_bold = false,
+      on_colors = function(colors) colors.border = '#7aa2f7' end,
+    }
+    vim.cmd.colorscheme 'tokyonight'
+  end,
+}
+
+themes.theme = themes.tokyonight
+themes.plug = themes.theme.plug
+themes.setup = themes.theme.setup
 
 return themes
