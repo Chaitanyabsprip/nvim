1279e4061435f5242d43226c1c04b3bcc5069799
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2022-12-27 03:49:16 +0530

    fix: completion select mapping and docs scroll mapping

--- lua/plugins/lsp/completion.lua
+++ lua/plugins/lsp/completion.lua
@@ -70,10 +70,10 @@ function completion.cmp.setup()
           fallback()
         end
       end, { 'i', 's' }),
-      ['<C-d>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
+      ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
       ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
-      ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(select_item_opts), { 'i', 'c' }),
-      ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(select_item_opts), { 'i', 'c' }),
+      ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
+      ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
       ['<C-Space>'] = cmp.mapping(cmp.mapping.complete {}, { 'i', 'c' }),
       ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
       ['<C-e>'] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
