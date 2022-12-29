4619acfb43869e94dfed459ce02502d0e07ba8f1
Chaitanya <chaitanya@zellerfeld.com>
2022-12-30 03:24:16 +0530

    update diagnositcs mappings (use ',' as a semantic mapping for diagnostics)

--- lua/lsp/diagnostics.lua
+++ lua/lsp/diagnostics.lua
@@ -24,17 +24,17 @@ function diagnostics.on_attach(_)
   local document_warnings = get_diagnostics(0, vim.diagnostic.severity.WARN)
   local document_infos = get_diagnostics(0, vim.diagnostic.severity.INFO)
   local document_hints = get_diagnostics(0, vim.diagnostic.severity.HINT)
-  nnoremap '<leader>dD'(get_diagnostics())(opts) 'Enlist all workspace diagnostics in quickfix'
-  nnoremap '<leader>dd'(get_diagnostics(0))(opts) 'Enlist all workspace diagnostics in quickfix'
-  nnoremap '<leader>dE'(workspace_errors)(opts) 'Enlist workspace error diagnostics in quickfix'
-  nnoremap '<leader>dW'(workspace_warnings)(opts) 'Enlist workspace warning diagnostics in quickfix'
-  nnoremap '<leader>dI'(workspace_infos)(opts) 'Enlist workspace info diagnostics in quickfix'
-  nnoremap '<leader>dH'(workspace_hints)(opts) 'Enlist workspace hint diagnostics in quickfix'
-  nnoremap '<leader>de'(document_errors)(opts) 'Enlist document error diagnostics in quickfix'
-  nnoremap '<leader>dw'(document_warnings)(opts) 'Enlist document warning diagnostics in quickfix'
-  nnoremap '<leader>di'(document_infos)(opts) 'Enlist document info diagnostics in quickfix'
-  nnoremap '<leader>dh'(document_hints)(opts) 'Enlist document hint diagnostics in quickfix'
-  nnoremap 'sd'(vim.diagnostic.open_float)(opts) 'Show current line diagnostics in a floating window'
+  nnoremap ',D'(get_diagnostics())(opts) 'Enlist all workspace diagnostics in quickfix'
+  nnoremap ',d'(get_diagnostics(0))(opts) 'Enlist all workspace diagnostics in quickfix'
+  nnoremap ',E'(workspace_errors)(opts) 'Enlist workspace error diagnostics in quickfix'
+  nnoremap ',W'(workspace_warnings)(opts) 'Enlist workspace warning diagnostics in quickfix'
+  nnoremap ',I'(workspace_infos)(opts) 'Enlist workspace info diagnostics in quickfix'
+  nnoremap ',H'(workspace_hints)(opts) 'Enlist workspace hint diagnostics in quickfix'
+  nnoremap ',e'(document_errors)(opts) 'Enlist document error diagnostics in quickfix'
+  nnoremap ',w'(document_warnings)(opts) 'Enlist document warning diagnostics in quickfix'
+  nnoremap ',i'(document_infos)(opts) 'Enlist document info diagnostics in quickfix'
+  nnoremap ',h'(document_hints)(opts) 'Enlist document hint diagnostics in quickfix'
+  nnoremap ',l'(vim.diagnostic.open_float)(opts) 'Show current line diagnostics in a floating window'
   nnoremap ',n' '<cmd>lua vim.diagnostic.goto_next()<cr>'(opts) 'Go to the next diagnostic'
   nnoremap ',p' '<cmd>lua vim.diagnostic.goto_prev()<cr>'(opts) 'Go to the previous diagnostic'
 end
