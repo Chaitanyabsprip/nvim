b2d83b2c6942328bf43e0c060d87957976b9c6a8
Chaitanya <chaitanya@zellerfeld.com>
2022-12-30 22:34:19 +0530

    add colorizer plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -4,6 +4,29 @@ misc.cellular_automation = {
   spec = { 'eandrju/cellular-automaton.nvim', event = 'VeryLazy' },
 }
 
+misc.colorizer = {
+  'NvChad/nvim-colorizer.lua',
+  cmd = { 'ColorizerAttachToBuffer', 'ColorizerToggle' },
+  config = {
+    filetypes = { '*', '!lazy' },
+    buftype = { '*', '!prompt', '!nofile' },
+    user_default_options = {
+      RGB = true, -- #RGB hex codes
+      RRGGBB = true, -- #RRGGBB hex codes
+      names = false, -- "Name" codes like Blue
+      RRGGBBAA = true, -- #RRGGBBAA hex codes
+      AARRGGBB = true, -- 0xAARRGGBB hex codes
+      rgb_fn = true, -- CSS rgb() and rgba() functions
+      hsl_fn = true, -- CSS hsl() and hsla() functions
+      css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+      css_fn = true, -- Enable all CSS *functions*: rgb_fn, hsl_fn
+      -- Available modes for `mode`: foreground, background,  virtualtext
+      mode = 'virtualtext', -- Set the display mode.
+      virtualtext = '███',
+    },
+  },
+}
+
 misc.drop = {
   spec = {
     'folke/drop.nvim',
@@ -46,6 +69,7 @@ misc.whichkey = {
 
 misc.spec = {
   misc.cellular_automation.spec,
+  misc.colorizer,
   misc.drop.spec,
   misc.fish.spec,
   misc.startuptime.spec,
