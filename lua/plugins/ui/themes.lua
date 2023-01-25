dfcf2cb33f886120efac8741c5ecd6d2a38d4fef
Chaitanya <chaitanya@zellerfeld.com>
2023-01-25 23:46:17 +0530

    feat: updated themes, and removed unnecessary

--- lua/plugins/ui/themes.lua
+++ lua/plugins/ui/themes.lua
@@ -1,150 +1,202 @@
 local themes = {}
-local config = require 'plugins.ui.config'
+local config = function() return require 'plugins.ui.config' end
 
-themes.blue_moon = {
-  spec = {
-    'kyazdani42/blue-moon',
-    config = function() require('plugins.ui.themes').blue_moon.setup() end,
-    lazy = false,
-  },
-  setup = function() vim.cmd.colorscheme 'blue-moon' end,
-}
-
-themes.calvera = {
-  spec = {
-    'yashguptaz/calvera-dark.nvim',
-    config = function() require('plugins.ui.themes').calvera.setup() end,
-    lazy = false,
-  },
-  setup = function()
-    vim.g.calvera_italic_comments = config.theme.italics.comments
-    vim.g.calvera_italic_keywords = config.theme.italics.keywords
-    vim.g.calvera_italic_functions = config.theme.italics.functions
-    vim.g.calvera_italic_variables = config.theme.italics.variables
-    vim.g.calvera_borders = true
-    vim.g.calvera_contrast = true
-    vim.g.calvera_hide_eob = true
-    vim.g.calvera_disable_background = config.theme.transparent
-    vim.g.transparent_bg = config.theme.transparent
-    require('calvera').set()
-  end,
-}
+local t = function() return require 'plugins.ui.themes' end
 
 themes.catppuccin = {
   spec = {
     'catppuccin/nvim',
     name = 'catppuccin',
-    config = function() require('plugins.ui.themes').setup() end,
     lazy = false,
+    config = function() t().catppuccin.setup() end,
   },
   setup = function()
     require('catppuccin').setup {
       flavor = 'mocha',
       term_colors = true,
+      transparent_background = true,
+      custom_highlights = function(c)
+        return {
+          CmpItemKindConstructor = { fg = c.base, bg = c.blue },
+          CmpItemKindEvent = { fg = c.base, bg = c.blue },
+          CmpItemKindFile = { fg = c.base, bg = c.blue },
+          CmpItemKindFolder = { fg = c.base, bg = c.blue },
+          CmpItemKindFunction = { fg = c.base, bg = c.blue },
+          CmpItemKindMethod = { fg = c.base, bg = c.blue },
+          CmpItemKindModule = { fg = c.base, bg = c.blue },
+          CmpItemKindOperator = { fg = c.base, bg = c.blue },
+          CmpItemKindStruct = { fg = c.base, bg = c.blue },
+          CmpItemKindTypeParameter = { fg = c.base, bg = c.blue },
+          CmpItemKindEnum = { fg = c.base, bg = c.green },
+          CmpItemKindField = { fg = c.base, bg = c.green },
+          CmpItemKindProperty = { fg = c.base, bg = c.green },
+          CmpItemKindUnit = { fg = c.base, bg = c.green },
+          CmpItemKindKeyword = { fg = c.base, bg = c.red },
+          CmpItemKindReference = { fg = c.base, bg = c.red },
+          CmpItemKindEnumMember = { fg = c.base, bg = c.red },
+          CmpItemKindColor = { fg = c.base, bg = c.red },
+          CmpItemKindClass = { fg = c.base, bg = c.yellow },
+          CmpItemKindInterface = { fg = c.base, bg = c.yellow },
+          CmpItemKindValue = { fg = c.base, bg = c.peach },
+          CmpItemKindConstant = { fg = c.base, bg = c.peach },
+          CmpItemKindCopilot = { fg = c.base, bg = c.teal },
+          CmpItemKindText = { fg = c.base, bg = c.teal },
+          CmpItemKindSnippet = { fg = c.base, bg = c.mauve },
+          CmpItemKindVariable = { fg = c.base, bg = c.flamingo },
+          LeapBackdrop = { fg = '#545c7e' },
+        }
+      end,
       integrations = {
-        treesitter = true,
         gitsigns = true,
-        telescope = true,
-        nvimtree = { enabled = true, show_root = true },
+        harpoon = true,
+        leap = true,
         markdown = true,
+        mini = true,
+        navic = { enabled = true },
+        neotest = true,
+        noice = true,
+        notify = true,
+        nvimtree = { enabled = true, show_root = true },
+        semantic_tokens = true,
+        treesitter = true,
+        ts_rainbow = true,
       },
     }
     vim.cmd.colorscheme 'catppuccin'
   end,
+  set = function() vim.cmd.colorscheme 'catppuccin' end,
 }
 
-themes.horizon = {
+themes.github = {
   spec = {
-    'LunarVim/horizon.nvim',
-    config = function() require('plugins.ui.themes').horizon.setup() end,
-  },
-  setup = function() vim.cmd [[ colorscheme horizon ]] end,
-}
-
-themes.material = {
-  spec = {
-    'marko-cerovac/material.nvim',
-    config = function() require('plugins.ui.themes').material.setup() end,
+    'projekt0n/github-nvim-theme',
+    version = '*',
+    config = function() t().github.setup() end,
     lazy = false,
   },
   setup = function()
-    vim.g.material_style = 'deep ocean'
-    require('material').setup {
-      contrast = {
-        popup_menu = true,
-        sidebars = true,
-        floating_windows = true,
-        cursor_line = true,
-      },
-      italics = {
-        comments = true,
-        keywords = true,
-        functions = true,
-        variables = true,
-      },
-      contrast_filetypes = config.theme.sidebars,
-      disable = {
-        eob_lines = config.theme.hide_eob,
-        background = config.theme.transparent,
-      },
+    require('github-theme').setup {
+      theme_style = 'dark_default',
+      dark_float = true,
+      sidebars = { 'qf', 'terminal', 'nvimtree', 'telescope', 'harpoon', 'neotest' },
+      dev = true,
+      overrides = function(c)
+        return {
+          CmpItemKindConstructor = { fg = c.base, bg = c.blue },
+          CmpItemKindEvent = { fg = c.base, bg = c.blue },
+          CmpItemKindFile = { fg = c.base, bg = c.blue },
+          CmpItemKindFolder = { fg = c.base, bg = c.blue },
+          CmpItemKindFunction = { fg = c.base, bg = c.blue },
+          CmpItemKindMethod = { fg = c.base, bg = c.blue },
+          CmpItemKindModule = { fg = c.base, bg = c.blue },
+          CmpItemKindOperator = { fg = c.base, bg = c.blue },
+          CmpItemKindStruct = { fg = c.base, bg = c.blue },
+          CmpItemKindTypeParameter = { fg = c.base, bg = c.blue },
+          CmpItemKindEnum = { fg = c.base, bg = c.green },
+          CmpItemKindField = { fg = c.base, bg = c.green },
+          CmpItemKindProperty = { fg = c.base, bg = c.green },
+          CmpItemKindUnit = { fg = c.base, bg = c.green },
+          CmpItemKindKeyword = { fg = c.base, bg = c.red },
+          CmpItemKindReference = { fg = c.base, bg = c.red },
+          CmpItemKindEnumMember = { fg = c.base, bg = c.red },
+          CmpItemKindColor = { fg = c.base, bg = c.red },
+          CmpItemKindClass = { fg = c.base, bg = c.yellow },
+          CmpItemKindInterface = { fg = c.base, bg = c.yellow },
+          CmpItemKindValue = { fg = c.base, bg = c.peach },
+          CmpItemKindConstant = { fg = c.base, bg = c.peach },
+          CmpItemKindCopilot = { fg = c.base, bg = c.teal },
+          CmpItemKindText = { fg = c.base, bg = c.teal },
+          CmpItemKindSnippet = { fg = c.base, bg = c.mauve },
+          CmpItemKindVariable = { fg = c.base, bg = c.flamingo },
+          LeapBackdrop = { fg = '#545c7e' },
+        }
+      end,
     }
-    vim.cmd [[ colorscheme material ]]
   end,
-}
-
-themes.nordoir = {
-  spec = {
-    'jesseleite/nvim-noirbuddy',
-    dependencies = { 'tjdevries/colorbuddy.nvim', branch = 'dev' },
-    config = function() require('plugins.ui.themes').nordoir.setup() end,
-    lazy = false,
-  },
-  setup = function() require('noirbuddy').setup { preset = 'miami-nights' } end,
+  set = function() end,
 }
 
 themes.rosepine = {
   spec = {
     'rose-pine/neovim',
+    -- 'denspiro/rose-pine',
+    -- 'sindrets/rose-pine-neovim',
     name = 'rose-pine',
     config = function() require('plugins.ui.themes').rosepine.setup() end,
     lazy = false,
   },
   setup = function()
-    vim.g.rose_pine_variant = 'base'
-    vim.g.rose_pine_enable_italics = true
-    vim.g.rose_pine_disable_background = config.theme.transparent
-    vim.cmd.colorscheme 'rose-pine'
-  end,
-}
+    vim.o.background = 'dark'
+    require('rose-pine').setup {
+      dark_variant = 'main',
+      disable_background = true,
+      highlight_groups = {
+        CmpItemAbbr = { fg = 'subtle' },
+        CmpItemAbbrDeprecated = { fg = 'subtle', style = 'strikethrough' },
+        CmpItemAbbrMatch = { fg = 'iris', style = 'bold' },
+        CmpItemAbbrMatchFuzzy = { fg = 'iris', style = 'bold' },
+        CmpItemKind = { fg = 'base', bg = 'iris' },
+        CmpItemKindClass = { fg = 'base', bg = 'gold' },
+        CmpItemKindColor = { fg = 'base', bg = 'love' },
+        CmpItemKindConstant = { fg = 'base', bg = 'rose' },
+        CmpItemKindConstructor = { fg = 'base', bg = 'foam' },
+        CmpItemKindCopilot = { fg = 'base', bg = 'iris' },
+        CmpItemKindEnum = { fg = 'base', bg = 'pine' },
+        CmpItemKindEnumMember = { fg = 'base', bg = 'love' },
+        CmpItemKindEvent = { fg = 'base', bg = 'foam' },
+        CmpItemKindField = { fg = 'base', bg = 'pine' },
+        CmpItemKindFile = { fg = 'base', bg = 'foam' },
+        CmpItemKindFolder = { fg = 'base', bg = 'foam' },
+        CmpItemKindFunction = { fg = 'base', bg = 'iris' },
+        CmpItemKindInterface = { fg = 'base', bg = 'gold' },
+        CmpItemKindKeyword = { fg = 'base', bg = 'love' },
+        CmpItemKindMethod = { fg = 'base', bg = 'iris' },
+        CmpItemKindModule = { fg = 'base', bg = 'foam' },
+        CmpItemKindOperator = { fg = 'base', bg = 'foam' },
+        CmpItemKindProperty = { fg = 'base', bg = 'pine' },
+        CmpItemKindReference = { fg = 'base', bg = 'love' },
+        CmpItemKindSnippet = { fg = 'base', bg = 'iris' },
+        CmpItemKindStruct = { fg = 'base', bg = 'foam' },
+        CmpItemKindText = { fg = 'base', bg = 'iris' },
+        CmpItemKindTypeParameter = { fg = 'base', bg = 'foam' },
+        CmpItemKindUnit = { fg = 'base', bg = 'pine' },
+        CmpItemKindValue = { fg = 'base', bg = 'rose' },
+        CmpItemKindVariable = { fg = 'base', bg = 'foam' },
+        TelescopeBorder = { fg = 'surface', bg = 'surface' },
+        TelescopeNormal = { bg = 'surface', fg = 'muted' },
+        TelescopePromptCounter = { fg = 'text', bg = 'base' },
+        TelescopePromptBorder = { fg = 'base', bg = 'base' },
+        TelescopePromptNormal = { fg = 'text', bg = 'base', style = 'bold' },
+        TelescopePromptPrefix = { fg = 'gold' },
+        TelescopeMatching = { fg = 'text', style = 'bold' },
+        TelescopeSelection = { fg = 'muted', bg = 'overlay' },
+        TelescopeSelectionCaret = { fg = 'rose', bg = 'overlay' },
+        TelescopeTitle = { fg = 'muted', bg = 'love' },
+        TelescopePreviewTitle = { fg = 'base', bg = 'iris' },
+        TelescopePromptTitle = { fg = 'base', bg = 'love' },
+        TelescopeResultsTitle = { fg = 'base', bg = 'foam' },
+        NotifyBackground = { bg = 'base' },
+        Drop = {},
 
-themes.sakura = {
-  spec = {
-    'numToStr/Sakura.nvim',
-    config = function() require('plugins.ui.themes').sakura.setup() end,
-    lazy = false,
-  },
-  setup = function()
-    local sakura = require 'sakura'
-    sakura.setup {
-      variant = 'moon',
-      transparent = config.theme.transparent,
-      italics = true,
+        LeapBackdrop = { fg = '#545c7e' },
+      },
     }
-    sakura.load()
+    vim.cmd.colorscheme 'rose-pine'
   end,
 }
 
 themes.tokyonight = {
   spec = {
     'folke/tokyonight.nvim',
-    config = function() require('plugins.ui.themes').tokyonight.setup() end,
-    lazy = false,
+    -- lazy = false,
+    config = function() t().tokyonight.setup() end,
+    event = 'VeryLazy',
   },
   setup = function()
     local tokyonight = require 'tokyonight'
     tokyonight.setup {
       style = 'night',
-      transparent = config.theme.transparent,
+      transparent = false,
       terminal_colors = true,
       styles = {
         comments = 'italic',
@@ -154,17 +206,28 @@ themes.tokyonight = {
         sidebars = 'dark',
         floats = 'dark',
       },
-      sidebars = config.theme.sidebars,
+      sidebars = config().theme.sidebars,
       day_brightness = 0.3,
       hide_inactive_statusline = false,
       dim_inactive = false,
       lualine_bold = false,
+      on_highlights = function(hl, c)
+        hl.NotifyBackground = { bg = c.bg, fg = c.fg }
+        hl.Cursor = { fg = c.black, bg = c.fg }
+      end,
       on_colors = function(colors) colors.border = '#7aa2f7' end,
     }
-    tokyonight.load()
+    -- tokyonight.load()
   end,
 }
 
-themes.spec = themes.tokyonight.spec
+themes.setup = function() themes[config().theme.name].set() end
+
+themes.spec = {
+  -- themes.catppuccin.spec,
+  -- themes.github.spec,
+  themes.rosepine.spec,
+  themes.tokyonight.spec,
+}
 
 return themes
