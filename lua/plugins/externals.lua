3522857723806a0f062bbf36efcd8aff2d865652
Chaitanya <chaitanya@zellerfeld.com>
2023-07-02 03:47:26 +0530

    fix: use lazy key trigger for terminal

--- lua/plugins/externals.lua
+++ lua/plugins/externals.lua
@@ -13,7 +13,7 @@ externals.toggleterm = {
         desc = 'Toggle floating terminal',
       },
       {
-        '<leader>tg',
+        '<c-g>',
         function() require('plugins.externals').gitui():toggle() end,
         noremap = true,
         desc = 'Toggle gitui in floating window',
@@ -38,17 +38,14 @@ externals.toggleterm = {
         highlights = { border = 'Normal', background = 'Normal' },
       },
     }
-    local ext = require 'plugins.externals'
-    local nnoremap = require('hashish').nnoremap
-    local opts = { silent = true }
-    nnoremap '<leader>tf'(function() ext.terminal(true):toggle() end)(opts) 'Toggle floating terminal'
-    nnoremap '<c-g>'(function() ext.gitui(true):toggle() end)(opts) 'Toggle gitui in floating window'
   end,
 }
 
 function externals.terminal(floating)
   floating = floating or false
   local Terminal = require('toggleterm.terminal').Terminal
+  local nnoremap = require('hashish').nnoremap
+  local tnoremap = require('hashish').tnoremap
   local float_opts = {}
   local direction = 'horizontal'
   if floating then
@@ -61,11 +58,9 @@ function externals.terminal(floating)
     float_opts = float_opts,
     on_open = function(term)
       vim.cmd 'startinsert!'
-      local nnoremap = require('hashish').nnoremap
       nnoremap 'q' '<cmd>close<cr>' { silent = true, bufnr = term.bufnr } 'Close Terminal Window'
     end,
     on_close = function()
-      local tnoremap = require('hashish').tnoremap
       tnoremap 'jk' '<C-\\><C-N>' 'jk as escape'
       tnoremap 'kj' '<C-\\><C-N>' 'kj as escape'
     end,
@@ -74,19 +69,19 @@ end
 
 function externals.gitui()
   local Terminal = require('toggleterm.terminal').Terminal
+  local nnoremap = require('hashish').nnoremap
+  local tnoremap = require('hashish').tnoremap
   return Terminal:new {
     cmd = 'gitui',
     close_on_exit = true,
     dir = 'git_dir',
     direction = 'float',
     on_open = function(term)
-      local nnoremap = require('hashish').nnoremap
       nnoremap 'q' '<cmd>close<cr>' { silent = true, bufnr = term.bufnr } 'Close Terminal Window'
       vim.api.nvim_del_keymap('t', 'jk')
       vim.api.nvim_del_keymap('t', 'kj')
     end,
     on_close = function()
-      local tnoremap = require('hashish').tnoremap
       tnoremap 'jk' '<C-\\><C-N>' 'jk as escape'
       tnoremap 'kj' '<C-\\><C-N>' 'kj as escape'
     end,
