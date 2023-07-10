e1d564e99bc40815b2000094fcbd5692b1c6d341
Chaitanya <chaitanya@zellerfeld.com>
2023-07-10 17:15:04 +0530

    feat(markdown): add conceal for todo lists

---
+++ queries/markdown/highlights.scm
@@ -0,0 +1,17 @@
+;; extends
+
+(list
+ (list_item
+  (list_marker_minus) @conceal (#set! conceal "")
+  (task_list_marker_checked)
+  )) 
+
+(list
+ (list_item
+  (list_marker_minus) @conceal (#set! conceal "")
+  (task_list_marker_unchecked)
+  )) 
+
+([(task_list_marker_checked)] @conceal (#set! conceal "✔"))
+
+([(task_list_marker_unchecked)] @conceal (#set! conceal "☐"))
