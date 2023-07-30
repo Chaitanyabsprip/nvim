60e7187cb8b9cc40e2f7ecd35d43ec47b7c15ec4
Chaitanya <chaitanya@zellerfeld.com>
2023-07-30 17:36:41 +0530

    make a plugin to interact with semantic search for my notes

---
+++ lua/semantic_search/utils.lua
@@ -0,0 +1,16 @@
+local utils = {}
+local Job = require 'plenary.job'
+
+function utils.execute(command, args)
+  local job = Job:new { command = command, args = args }
+  job:sync(10000)
+  return job
+end
+
+function utils.exec_async(command, args, on_exit)
+  local job = Job:new { command = command, args = args, on_exit = on_exit }
+  job:start()
+  return job
+end
+
+return utils
