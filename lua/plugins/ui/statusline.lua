00e0a0de859006a5dacd562d82140da6bcbc1021
Chaitanya <chaitanya@zellerfeld.com>
2023-08-01 17:46:01 +0530

    simplify lualine config

--- lua/plugins/ui/statusline.lua
+++ lua/plugins/ui/statusline.lua
@@ -1,22 +1,20 @@
 ---@diagnostic disable: undefined-field
 local statusline = {}
 
-local function get_lsp_client(msg)
-  msg = msg or 'No Active Lsp'
-  local buf_ft = vim.api.nvim_get_option_value('filetype', { buf = 0 })
+local function get_lsp_client(_)
+  local msg = 'No Active Lsp'
+  local filetype = vim.api.nvim_get_option_value('filetype', { buf = 0 })
   local clients = vim.lsp.get_clients()
   if next(clients) == nil then return msg end
   for _, client in ipairs(clients) do
     local filetypes = client.config.filetypes
     local client_name = client.name
-    if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 and client_name ~= 'null-ls' then
-      return '  ' .. client.name
+    if filetypes and vim.fn.index(filetypes, filetype) ~= -1 and client_name ~= 'null-ls' then
+      return client.name or msg
     end
   end
   return msg
 end
-
-local function clock() return ' ' .. os.date '%H:%M' end
 
 statusline.lualine = {
   'nvim-lualine/lualine.nvim',
@@ -30,13 +28,15 @@ statusline.lualine = {
     },
     sections = {
       lualine_a = { 'branch' },
-      lualine_b = { get_lsp_client },
-      lualine_c = {},
+      lualine_b = {},
+      lualine_c = {
+        { get_lsp_client, icon = '', on_click = function() vim.cmd [[LspInfo]] end },
+      },
       lualine_x = { 'filetype' },
-      lualine_y = { 'location' },
-      lualine_z = { { clock, color = { gui = 'bold' } } },
+      lualine_y = {},
+      lualine_z = { { 'datetime', style = '%H:%M', icon = '', color = { gui = 'bold' } } },
     },
-    extensions = { 'nvim-tree', 'toggleterm' },
+    extensions = { 'lazy', 'nvim-dap-ui', 'nvim-tree', 'toggleterm', 'quickfix' },
   },
 }
 
