26051138dee1884df920d616222fefd1d33374f6
Chaitanya <chaitanya.sharma@yocket.com>
2023-09-18 03:40:31 +0530

    feat: add oil.nvim plugin

--- lua/plugins/explorer/init.lua
+++ lua/plugins/explorer/init.lua
@@ -66,117 +66,111 @@ explorer.harpoon = {
   },
   opts = { global_settings = { mark_branch = true } },
 }
-local function on_attach(bufnr)
-  local api = require 'nvim-tree.api'
-  local function opts(desc)
-    return {
-      desc = 'nvim-tree: ' .. desc,
-      buffer = bufnr,
-      noremap = true,
-      silent = true,
-      nowait = true,
-    }
-  end
-  local function grep_at_current_tree_node()
-    local node = require('nvim-tree.lib').get_node_at_cursor()
-    if not node then return end
-    require('telescope.builtin').live_grep { search_dirs = { node.absolute_path } }
-  end
-  api.config.mappings.default_on_attach(bufnr)
-  vim.keymap.del('n', ']e', { buffer = bufnr })
-  vim.keymap.del('n', '[e', { buffer = bufnr })
-  vim.keymap.set('n', 'l', api.node.open.edit, opts 'Open')
-  vim.keymap.set('n', 'cd', api.tree.change_root_to_node, opts 'CD')
-  vim.keymap.set('n', 'v', api.node.open.vertical, opts 'Open: Vertical Split')
-  vim.keymap.set('n', 's', api.node.open.horizontal, opts 'Open: Horizontal Split')
-  vim.keymap.set('n', 'h', api.node.navigate.parent_close, opts 'Close Directory')
-  vim.keymap.set('n', '<S-CR>', api.node.navigate.parent_close, opts 'Close Directory')
-  vim.keymap.set('n', 'o', api.node.run.system, opts 'Run System')
-  vim.keymap.set('n', ',n', api.node.navigate.diagnostics.next, opts 'Next Diagnostic')
-  vim.keymap.set('n', ',p', api.node.navigate.diagnostics.prev, opts 'Prev Diagnostic')
-  vim.keymap.set('n', 'gr', grep_at_current_tree_node, opts 'Telescope Live Grep at node')
-end
 
-explorer.nvim_tree = {
-  'nvim-tree/nvim-tree.lua',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  version = '*',
+explorer.oil = {
+  'stevearc/oil.nvim',
   event = 'VeryLazy',
+  cmd = { 'Oil', 'Explorer' },
   opts = function()
-    vim.api.nvim_create_user_command('Explorer', 'NvimTreeToggle', { nargs = 0 })
-    local group = vim.api.nvim_create_augroup('nvim-tree', { clear = true })
-    vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-      pattern = 'NvimTree*',
-      group = group,
-      callback = function() vim.cmd [[ setlocal statuscolumn= nonu nornu signcolumn=yes ]] end,
-    })
+    vim.api.nvim_create_user_command('Explorer', function()
+      if vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(0), 'filetype') == 'oil' then
+        require('oil').close()
+      else
+        return require('oil').open_float()
+      end
+    end, { nargs = 0 })
     return {
-      disable_netrw = true,
-      hijack_cursor = true,
-      hijack_netrw = true,
-      hijack_unnamed_buffer_when_opening = true,
-      reload_on_bufenter = true,
-      respect_buf_cwd = true,
-      on_attach = on_attach,
-      select_prompts = true,
-      view = {
-        centralize_selection = true,
-        cursorline = true,
-        width = 40,
-        side = 'right',
-        float = {
-          enable = true,
-          quit_on_focus_loss = true,
-          open_win_config = function()
-            local HEIGHT_RATIO = 0.9
-            local WIDTH_RATIO = 0.2
-            local screen_w = vim.opt.columns:get()
-            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-            local window_w = screen_w * WIDTH_RATIO
-            local window_h = screen_h * HEIGHT_RATIO
-            local window_w_int = math.floor(window_w)
-            local window_h_int = math.floor(window_h)
-            local center_x = (screen_w - window_w - (screen_w * 0.03))
-            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
-            return {
-              border = 'rounded',
-              relative = 'editor',
-              row = center_y,
-              col = center_x,
-              width = window_w_int,
-              height = window_h_int,
-            }
-          end,
-        },
+      default_file_explorer = true,
+      columns = { 'icon', 'permissions' }, -- See :help oil-columns
+      buf_options = { buflisted = false, bufhidden = 'hide' },
+      win_options = {
+        wrap = false,
+        signcolumn = 'no',
+        cursorcolumn = false,
+        foldcolumn = '0',
+        spell = false,
+        list = false,
+        conceallevel = 3,
+        concealcursor = 'nvic',
       },
-      renderer = {
-        add_trailing = true,
-        highlight_git = true,
-        full_name = true,
-        highlight_opened_files = 'name',
-        indent_width = 2,
-        indent_markers = { enable = true },
-        special_files = {
-          'Cargo.toml',
-          'Makefile',
-          'README.md',
-          'readme.md',
-          'readme.rst',
-          'README.rst',
-          'README',
-        },
+      delete_to_trash = false,
+      skip_confirm_for_simple_edits = false, -- Skip the confirmation popup for simple operations
+      trash_command = 'trash', -- Change this to customize the command used when deleting to trash
+      -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+      prompt_save_on_select_new_entry = true,
+      keymaps = { -- See :help oil-actions for a list of all available actions
+        ['g?'] = 'actions.show_help',
+        ['<CR>'] = 'actions.select',
+        ['L'] = 'actions.select',
+        ['s'] = 'actions.select_split',
+        ['S'] = 'actions.select_vsplit',
+        ['<C-t>'] = 'actions.select_tab',
+        ['<C-p>'] = 'actions.preview',
+        ['<C-c>'] = 'actions.close',
+        ['q'] = 'actions.close',
+        ['<C-l>'] = 'actions.refresh',
+        ['H'] = 'actions.parent',
+        ['_'] = 'actions.open_cwd',
+        ['`'] = 'actions.cd',
+        ['~'] = 'actions.tcd',
+        ['gs'] = 'actions.change_sort',
+        ['g.'] = 'actions.toggle_hidden',
       },
-      update_focused_file = { enable = true },
-      diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        show_on_open_dirs = false,
-        debounce_delay = 50,
+      use_default_keymaps = false, -- Set to false to disable all of the above keymaps
+      view_options = {
+        show_hidden = true, -- Show files and directories that start with "."
+        is_hidden_file = function(name, bufnr) return vim.startswith(name, '.') end,
+        is_always_hidden = function(name, bufnr) return false end,
+        sort = { { 'type', 'asc' }, { 'name', 'asc' } }, -- see :help oil-columns to see sortable cols
       },
-      modified = { enable = true },
-      trash = { cmd = 'trash' },
+      float = {
+        padding = 1,
+        max_width = 50,
+        max_height = 20,
+        border = 'rounded',
+        win_options = { winblend = 0 },
+        override = function(conf)
+          local HEIGHT_RATIO = 0.9
+          local WIDTH_RATIO = 0.3
+          local screen_w = vim.opt.columns:get()
+          local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+          local window_w = screen_w * WIDTH_RATIO
+          local window_h = screen_h * HEIGHT_RATIO
+          local window_w_int = math.floor(window_w)
+          local window_h_int = math.floor(window_h)
+          local center_x = (screen_w - window_w - (screen_w * 0.03))
+          local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+          conf.row = center_y
+          conf.col = center_x
+          conf.width = window_w_int
+          conf.height = window_h_int
+          return conf
+        end,
+      },
+      preview = {
+        max_width = { 80, 0.6 },
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 30, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = 'rounded',
+        win_options = { winblend = 0 },
+      },
+      progress = {
+        max_width = 0.9,
+        min_width = { 40, 0.4 },
+        width = nil,
+        max_height = { 10, 0.9 },
+        min_height = { 5, 0.1 },
+        height = nil,
+        border = 'rounded',
+        minimized_border = 'none',
+        win_options = { winblend = 0 },
+      },
     }
   end,
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
 }
 
 explorer.pretty_qf = {
@@ -188,7 +182,7 @@ explorer.pretty_qf = {
 explorer.spec = {
   explorer.better_qf,
   explorer.harpoon,
-  explorer.nvim_tree,
+  explorer.oil,
   explorer.pretty_qf,
 }
 
