178cc96305891a49c2be87ba013226114743390a
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-02 19:57:56 +0530

    feat(python): add plugin for better syntax highlight and indent

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -42,6 +42,39 @@ return {
             vim.list_extend(opts.ensure_installed, {
                 'python',
                 'toml',
+                'rst',
+                'ninja',
+                'markdown',
+                'markdown_inline',
+            })
+        end,
+    },
+    {
+        'wookayin/semshi', -- maintained fork
+        ft = 'python',
+        build = ':UpdateRemotePlugins', -- don't disable `rplugin` in lazy.nvim for this
+        init = function()
+            vim.g.python3_host_prog = vim.fn.exepath 'python3'
+            -- better done by LSP
+            vim.g['semshi#error_sign'] = false
+            vim.g['semshi#simplify_markup'] = false
+            vim.g['semshi#mark_selected_nodes'] = false
+            vim.g['semshi#update_delay_factor'] = 0.001
+
+            vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
+                callback = function()
+                    vim.cmd [[
+						highlight! semshiGlobal gui=italic
+						highlight! link semshiImported @lsp.type.namespace
+						highlight! link semshiParameter @lsp.type.parameter
+						highlight! link semshiParameterUnused DiagnosticUnnecessary
+						highlight! link semshiBuiltin @function.builtin
+						highlight! link semshiAttribute @field
+						highlight! link semshiSelf @lsp.type.selfKeyword
+						highlight! link semshiUnresolved @lsp.type.unresolvedReference
+						highlight! link semshiFree @comment
+					]]
+                end,
             })
         end,
     },
@@ -82,6 +115,7 @@ return {
         -- end,
     },
     {
+    { 'Vimjas/vim-python-pep8-indent' },
         'nvimtools/none-ls.nvim',
         ft = { 'python' },
         opts = function(_, opts)
