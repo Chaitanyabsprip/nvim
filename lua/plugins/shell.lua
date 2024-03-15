87e6a9aa7a0a35fae81a840a20f8d3daaef88e70
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-16 00:24:02 +0530

    feat: add shellcheck as a dependency for bash lsp

--- lua/plugins/shell.lua
+++ lua/plugins/shell.lua
@@ -15,7 +15,10 @@ return {
         'williamboman/mason.nvim',
         opts = function(_, opts)
             opts.ensure_installed = opts.ensure_installed or {}
-            vim.list_extend(opts.ensure_installed, { 'bash-language-server', 'shfmt' })
+            vim.list_extend(
+                opts.ensure_installed,
+                { 'bash-language-server', 'shfmt', 'shellcheck' }
+            )
         end,
     },
     {
