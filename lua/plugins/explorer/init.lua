5332142403f71a8ce74805f4699b938fdf9331df
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-17 00:44:41 +0530

    refactor: remove old harpoon code

--- lua/plugins/explorer/init.lua
+++ lua/plugins/explorer/init.lua
@@ -89,49 +89,6 @@ explorer.harpoon2 = {
     end,
 }
 
-explorer.harpoon = {
-    'ThePrimeagen/harpoon',
-    keys = {
-        {
-            '<c-b>',
-            function() require('harpoon.mark').add_file() end,
-            noremap = true,
-            desc = 'Add file to harpoon',
-        },
-        {
-            '<c-f>',
-            function() require('harpoon.ui').toggle_quick_menu() end,
-            noremap = true,
-            desc = 'Toggle harpoon marks list',
-        },
-        {
-            '<c-n>',
-            function() require('harpoon.ui').nav_file(1) end,
-            noremap = true,
-            desc = 'Jump to file 1 in harpoon',
-        },
-        {
-            '<c-e>',
-            function() require('harpoon.ui').nav_file(2) end,
-            noremap = true,
-            desc = 'Jump to file 2 in harpoon',
-        },
-        {
-            '<c-o>',
-            function() require('harpoon.ui').nav_file(3) end,
-            noremap = true,
-            desc = 'Jump to file 3 in harpoon',
-        },
-        {
-            '<c-s>',
-            function() require('harpoon.ui').nav_file(4) end,
-            noremap = true,
-            desc = 'Jump to file 4 in harpoon',
-        },
-    },
-    opts = { global_settings = { mark_branch = true } },
-}
-
 explorer.oil = {
     'stevearc/oil.nvim',
     lazy = (function()
