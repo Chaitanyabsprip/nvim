62f9ef329526a1ebe9d51fe78a339836796b7d97
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-03-19 02:38:47 +0530

    fix: remove semshi plugin

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -51,35 +51,6 @@ return {
         end,
     },
     {
-        'wookayin/semshi', -- maintained fork
-        ft = 'python',
-        build = ':UpdateRemotePlugins', -- don't disable `rplugin` in lazy.nvim for this
-        init = function()
-            vim.g.python3_host_prog = vim.fn.exepath 'python3'
-            -- better done by LSP
-            vim.g['semshi#error_sign'] = false
-            vim.g['semshi#simplify_markup'] = false
-            vim.g['semshi#mark_selected_nodes'] = false
-            vim.g['semshi#update_delay_factor'] = 0.001
-
-            vim.api.nvim_create_autocmd({ 'VimEnter', 'ColorScheme' }, {
-                callback = function()
-                    vim.cmd [[
-						highlight! semshiGlobal gui=italic
-						highlight! link semshiImported @lsp.type.namespace
-						highlight! link semshiParameter @lsp.type.parameter
-						highlight! link semshiParameterUnused DiagnosticUnnecessary
-						highlight! link semshiBuiltin @function.builtin
-						highlight! link semshiAttribute @field
-						highlight! link semshiSelf @lsp.type.selfKeyword
-						highlight! link semshiUnresolved @lsp.type.unresolvedReference
-						highlight! link semshiFree @comment
-					]]
-                end,
-            })
-        end,
-    },
-    {
         'williamboman/mason.nvim',
         opts = function(_, opts)
             opts.ensure_installed = opts.ensure_installed or {}
