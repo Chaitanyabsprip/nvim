0406c5fcb33c687764c6f95baac529a94c718a7f
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-04-12 02:36:57 +0530

    feat(golang): add more supported ft

--- lua/plugins/golang.lua
+++ lua/plugins/golang.lua
@@ -5,7 +5,7 @@ golang.treesitter = {
     'nvim-treesitter/nvim-treesitter',
     opts = function(_, opts)
         opts.ensure_installed = opts.ensure_installed or {}
-        vim.list_extend(opts.ensure_installed, { 'go' })
+        vim.list_extend(opts.ensure_installed, { 'go', 'gomod', 'gowork', 'gotmpl' })
     end,
 }
 
