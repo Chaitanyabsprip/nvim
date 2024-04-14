01562b2c1b076c804caed0b2c0b4dc3a1881f344
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-04-14 16:55:37 +0530

    feat: add rest.nvim plugin

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -96,10 +96,74 @@ tools.startuptime = {
     cmd = 'StartupTime',
 }
 
+tools.rest = {
+    'rest-nvim/rest.nvim',
+    ft = 'http',
+    event = 'VeryLazy',
+    dependencies = {
+        {
+            'vhyrro/luarocks.nvim',
+            priority = 1000,
+            config = true,
+        },
+        {
+            'nvim-treesitter/nvim-treesitter',
+            opts = function(_, opts)
+                opts.ensure_installed = opts.ensure_installed or {}
+                vim.list_extend(opts.ensure_installed, { 'lua', 'http', 'graphql', 'xml', 'json' })
+            end,
+        },
+    },
+    config = function()
+        require('rest-nvim').setup {
+            result = {
+                split = {
+                    horizontal = true,
+                },
+            },
+        }
+    end,
+}
+
+tools.hurl = {
+    'jellydn/hurl.nvim',
+    dependencies = {
+        'MunifTanjim/nui.nvim',
+        'nvim-lua/plenary.nvim',
+        {
+            'nvim-treesitter/nvim-treesitter',
+            opts = function(_, opts)
+                opts.ensure_installed = opts.ensure_installed or {}
+                vim.list_extend(opts.ensure_installed, { 'hurl' })
+            end,
+        },
+    },
+    ft = 'hurl',
+    event = 'VeryLazy',
+    opts = {
+        debug = false,
+        show_notification = false,
+        mode = 'split',
+        formatters = {
+            json = { 'jq' },
+            html = { 'prettierd', '--format', 'a.html' },
+        },
+    },
+    keys = {
+        -- { '<leader>A', '<cmd>HurlRunner<CR>', desc = 'Run All requests' },
+        -- { '<leader>a', '<cmd>HurlRunnerAt<CR>', desc = 'Run Api request' },
+        -- { '<leader>te', '<cmd>HurlRunnerToEntry<CR>', desc = 'Run Api request to entry' },
+        -- { '<leader>tm', '<cmd>HurlToggleMode<CR>', desc = 'Hurl Toggle Mode' },
+        -- { '<leader>tv', '<cmd>HurlVerbose<CR>', desc = 'Run Api in verbose mode' },
+        -- { '<leader>h', ':HurlRunner<CR>', desc = 'Hurl Runner', mode = 'v' },
+    },
+}
+
 tools.spec = {
     tools.colorizer,
     tools.obsidian,
     tools.peek,
+    tools.rest,
     tools.startuptime,
 }
 
