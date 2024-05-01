18bb7971dbe773e4ae66953fcf873ebe33b4d4e9
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-05-01 16:33:45 +0530

    feat(awk): add lsp support for awk lang

--- lua/plugins/shell.lua
+++ lua/plugins/shell.lua
@@ -2,6 +2,7 @@ local extend = require('plugins.lsp').extend
 local function bashls(lspconfig)
     lspconfig.bashls.setup(extend { filetypes = { 'bash', 'zsh', 'sh' } })
 end
+local function awkls(lspconfig) lspconfig.awk_ls.setup(extend {}) end
 
 return {
     {
@@ -17,7 +18,7 @@ return {
             opts.ensure_installed = opts.ensure_installed or {}
             vim.list_extend(
                 opts.ensure_installed,
-                { 'bash-language-server', 'shfmt', 'shellcheck' }
+                { 'bash-language-server', 'shfmt', 'shellcheck', 'awk-language-server' }
             )
         end,
     },
@@ -37,6 +38,6 @@ return {
             })
         end,
     },
-    { 'neovim/nvim-lspconfig', opts = { servers = { bashls = bashls } } },
+    { 'neovim/nvim-lspconfig', opts = { servers = { bashls = bashls, awkls = awkls } } },
     { 'dag/vim-fish', ft = 'fish', cond = function() return vim.loop.fs_stat 'config.fish' end },
 }
