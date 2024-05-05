00d8cf03d630e19e01515fc6451a086edfc0236c
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-05-06 03:37:49 +0530

    feat(go): add leetcode compiler plugin

---
+++ after/compiler/leetcode.lua
@@ -0,0 +1,64 @@
+--TODO(chaitanya): make this work with golang
+---@diagnostic disable: missing-fields
+local function new_popup()
+    local Popup = require 'plenary.popup'
+    vim.api.nvim_set_hl(0, 'RunNormal', { background = '#0F111A' })
+    vim.api.nvim_set_hl(0, 'RunTitle', { background = '#7AB0DF', foreground = '#0F111A' })
+    local height = 10
+    local width = 60
+    local buffer = vim.api.nvim_create_buf(false, false)
+    return {
+        buffer = buffer,
+        open = function()
+            return Popup.create(buffer, {
+                title = 'Run',
+                highlight = 'RunNormal,FloatTitle:RunTitle',
+                line = math.floor(((vim.o.lines - height) / 2) - 1),
+                col = math.floor((vim.o.columns - width) / 2),
+                minwidth = width,
+                minheight = height,
+                border = {},
+                borderchars = { '─', '│', '─', '│', '╭', '╮', '╯', '╰' },
+            })
+        end,
+    }
+end
+
+vim.api.nvim_create_user_command('Run', function()
+    local popup = new_popup()
+    local Task = require 'plenary.job'
+    local file = vim.fn.expand '%'
+    local stem = vim.fn.fnamemodify(file, ':t:r')
+    local window, _ = popup.open()
+
+    local channel = vim.api.nvim_open_term(popup.buffer, {})
+    vim.api.nvim_win_set_option(window, 'number', false)
+    local task = Task:new {
+        './' .. stem,
+        skip_validation = true,
+        on_stdout = vim.schedule_wrap(function(errors, data, _)
+            if errors then return end
+            if not data then return vim.api.nvim_chan_send(channel, '=====END=====\r\n') end
+            pcall(vim.api.nvim_chan_send, channel, data .. '\r\n')
+        end),
+        on_exit = vim.schedule_wrap(function(_, __) vim.cmd('!rm ' .. stem) end),
+    }
+    Task:new({
+        'go',
+        'build',
+        '-o',
+        stem,
+        file,
+        on_exit = function(_, code)
+            if code ~= 0 then return end
+            task:start()
+        end,
+    }):start()
+    vim.keymap.set('n', '<C-q>', function() task:shutdown() end, { buffer = popup.buffer })
+    vim.keymap.set(
+        'n',
+        'q',
+        function() vim.api.nvim_win_close(window, true) end,
+        { buffer = popup.buffer }
+    )
+end, { desc = 'Compile and run C file.' })
--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,2 +1,3 @@
 vim.opt_local.colorcolumn = '101'
 vim.opt_local.textwidth = 100
+vim.cmd [[compiler leetcode]]
