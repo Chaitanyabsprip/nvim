c105992ebe41519d1d3a4b84110b8083d7a56a76
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-05-21 03:38:44 +0530

    feat(database): add vim dadbob and sibling plugins

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,6 +1,45 @@
 return {
     ---@type LazyPluginSpec
     {
+        'kristijanhusak/vim-dadbod-completion',
+        dependencies = { 'hrsh7th/nvim-cmp' },
+        config = function()
+            require('cmp').setup.filetype({ 'sql' }, {
+                sources = {
+                    { name = 'vim-dadbod-completion' },
+                    { name = 'buffer' },
+                },
+            })
+        end,
+    },
+    ---@type LazyPluginSpec
+    {
+        'kristijanhusak/vim-dadbod-ui',
+        dependencies = { 'tpope/vim-dadbod' },
+        config = function()
+            vim.g.db_ui_use_nerd_fonts = true
+            vim.g.db_ui_show_database_icon = true
+            vim.g.db_ui_use_nvim_notify = true
+        end,
+        cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
+        keys = {
+            {
+                '<leader>dt',
+                '<cmd>DBUIToggle<cr>',
+                noremap = true,
+                silent = true,
+                desc = 'DB: Toggle database ui',
+            },
+        },
+    },
+    ---@type LazyPluginSpec
+    {
+        'tpope/vim-dadbod',
+        dependencies = { 'kristijanhusak/vim-dadbod-completion' },
+        cmd = 'DB',
+    },
+    ---@type LazyPluginSpec
+    {
         'kndndrj/nvim-dbee',
         dependencies = {
             'MunifTanjim/nui.nvim',
