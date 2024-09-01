3d40918dbab0b9fd24eaa566c26ad9f1a899d554
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-01 16:12:16 +0530

    style: remove commented code

--- lua/plugins/python.lua
+++ lua/plugins/python.lua
@@ -75,29 +75,6 @@ return {
                 'debugpy' -- debugger
             )
         end,
-        -- config = function(_, opts)
-        --     require('mason').setup(opts)
-        --     local pylsp = require('mason-registry').get_package 'python-lsp-server'
-        --     pylsp:on('install:success', function()
-        --         local function mason_package_path(package)
-        --             local path =
-        --                 vim.fn.resolve(vim.fn.stdpath 'data' .. '/mason/packages/' .. package)
-        --             return path
-        --         end
-        --
-        --         local path = mason_package_path 'python-lsp-server'
-        --         local command = path .. '/venv/bin/pip'
-        --         local args = { 'install', 'pylsp-rope' }
-        --
-        --         require('plenary.job')
-        --             :new({
-        --                 command = command,
-        --                 args = args,
-        --                 cwd = path,
-        --             })
-        --             :start()
-        --     end)
-        -- end,
     },
     {
         'mfussenegger/nvim-dap-python',
