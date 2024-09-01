09afa3b075efe02c55de218295680d572f48617a
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-01 16:09:24 +0530

    fix: don't update tabstop

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -128,10 +128,9 @@ config.options.setup = function()
     vim.o.cindent = true
     vim.o.smarttab = true
     vim.o.expandtab = true
-    vim.o.shiftwidth = 4
     vim.o.smartindent = true
     vim.o.softtabstop = 4
-    vim.o.tabstop = 4
+    vim.o.shiftwidth = 4
 
     -- use undofile over swapfile
     vim.o.undodir = vim.fn.expand '~/.tmp/undodir'
