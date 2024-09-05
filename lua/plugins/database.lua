fe477257bc3ba25d240d33e407553c3e7bdb7989
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-05 11:36:26 +0530

    feat(dbui): use custom mappings

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -21,6 +21,31 @@ return {
             vim.g.db_ui_use_nerd_fonts = true
             vim.g.db_ui_show_database_icon = true
             vim.g.db_ui_use_nvim_notify = true
+            vim.g.db_ui_disable_mappings_dbui = true
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = 'dbui',
+                callback = function(args)
+                    ---@type integer
+                    local buffer = args.buf
+                    local opts = { buffer = buffer, noremap = true, silent = true }
+                    for _, val in ipairs { 'o', '<cr>', '<2-LeftMouse>' } do
+                        vim.keymap.set('n', val, '<Plug>(DBUI_SelectLine)', opts)
+                    end
+                    vim.keymap.set('n', '<c-v>', '<Plug>(DBUI_SelectLineVsplit)', opts)
+                    vim.keymap.set('n', 'R', '<Plug>(DBUI_Redraw)', opts)
+                    vim.keymap.set('n', 'd', '<Plug>(DBUI_DeleteLine)', opts)
+                    vim.keymap.set('n', 'A', '<Plug>(DBUI_AddConnection)', opts)
+                    vim.keymap.set('n', 'I', '<Plug>(DBUI_ToggleDetails)', opts)
+                    vim.keymap.set('n', 'r', '<Plug>(DBUI_RenameLine)', opts)
+                    vim.keymap.set('n', 'q', '<Plug>(DBUI_Quit)', opts)
+                    -- vim.keymap.set('n', 'F', '<Plug>(DBUI_GotoFirstSibling)', opts)
+                    -- vim.keymap.set('n', 'H', '<Plug>(DBUI_GotoLastSibling)', opts)
+                    vim.keymap.set('n', 'H', '<Plug>(DBUI_GotoParentNode)', opts)
+                    vim.keymap.set('n', 'L', '<Plug>(DBUI_GotoChildNode)', opts)
+                    vim.keymap.set('n', 'K', '<Plug>(DBUI_GotoPrevSibling)', opts)
+                    vim.keymap.set('n', 'J', '<Plug>(DBUI_GotoNextSibling', opts)
+                end,
+            })
         end,
         cmd = { 'DBUI', 'DBUIToggle', 'DBUIAddConnection', 'DBUIFindBuffer' },
         keys = {
@@ -54,6 +79,7 @@ return {
         config = function()
             require('dbee').setup(--[[optional config]])
         end,
+        enabled = false,
         keys = {
             {
                 '<leader>db',
