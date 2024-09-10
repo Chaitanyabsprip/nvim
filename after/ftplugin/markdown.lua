17942a7b5ae37f48ce9bd13aa48dad16c4b6b847
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-10 13:53:33 +0530

    feat(markdown): remove spell opt

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -3,7 +3,6 @@ vim.opt_local.smartindent = false
 vim.opt_local.smarttab = true
 vim.opt_local.shiftwidth = 2
 vim.opt_local.softtabstop = 2
-vim.opt_local.spell = true
 vim.opt_local.formatoptions:append 'p'
 vim.opt_local.comments = 'n:>'
 vim.opt_local.list = true
@@ -24,14 +23,14 @@ local formatlistpat = { -- (^\s*\d+[\.)\]}]\s+|^\s*[-*+]\s(\[[x\s]\]\s)?)
 }
 vim.opt_local.formatlistpat = table.concat(formatlistpat, '')
 
-local group = vim.api.nvim_create_augroup('markdown-fold', { clear = true })
-vim.api.nvim_create_autocmd('BufReadPost', {
-    group = group,
-    pattern = '*.md',
-    callback = function(event)
-        vim.api.nvim_create_autocmd(
-            'BufWritePost',
-            { group = group, buffer = event.buf, command = '1 foldc' }
-        )
-    end,
-})
+-- local group = vim.api.nvim_create_augroup('markdown-fold', { clear = true })
+-- vim.api.nvim_create_autocmd('BufReadPost', {
+--     group = group,
+--     pattern = '*.md',
+--     callback = function(event)
+--         vim.api.nvim_create_autocmd(
+--             'BufWritePost',
+--             { group = group, buffer = event.buf, command = '1 foldc' }
+--         )
+--     end,
+-- })
