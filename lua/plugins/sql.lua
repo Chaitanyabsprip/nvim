58a42f975e19b0b3b29124846db95418f770cf66
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-21 04:49:36 +0530

    feat: add sqlfluff back

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -11,7 +11,13 @@ return {
         'williamboman/mason.nvim',
         optional = true,
         opts = function(_, opts)
-            require('config.lazy').extend_opts_list(opts, 'ensure_installed', 'sqlfluff')
+            require('config.lazy').extend_opts_list(
+                opts,
+                'ensure_installed',
+                'sqlfluff',
+                'sqlfmt'
+                -- 'sql-formatter'
+            )
         end,
     },
     {
@@ -27,7 +33,7 @@ return {
                     local fluff_opts =
                         { extra_args = { '--dialect', 'postgres', '--exclude-rules', 'RF05' } }
                     return {
-                        -- builtins.diagnostics.sqlfluff.with(fluff_opts),
+                        builtins.diagnostics.sqlfluff.with(fluff_opts),
                         -- builtins.formatting.sqlfluff.with(fluff_opts),
                         -- builtins.formatting.pg_format,
                         -- builtins.formatting.sql_formatter,
