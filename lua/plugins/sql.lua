532d3dac52b0363c615d0af98204c3eb6f737c31
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-20 13:30:59 +0530

    feat(sql): use sqlfmt for formatting

--- lua/plugins/sql.lua
+++ lua/plugins/sql.lua
@@ -27,8 +27,11 @@ return {
                     local fluff_opts =
                         { extra_args = { '--dialect', 'postgres', '--exclude-rules', 'RF05' } }
                     return {
-                        builtins.diagnostics.sqlfluff.with(fluff_opts),
-                        builtins.formatting.sqlfluff.with(fluff_opts),
+                        -- builtins.diagnostics.sqlfluff.with(fluff_opts),
+                        -- builtins.formatting.sqlfluff.with(fluff_opts),
+                        -- builtins.formatting.pg_format,
+                        -- builtins.formatting.sql_formatter,
+                        builtins.formatting.sqlfmt,
                     }
                 end
             )
