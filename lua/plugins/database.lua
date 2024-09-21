602d1469e02bcb8222fd85230348f1c941b471e9
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-09-22 02:23:03 +0530

    style: format code

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -81,9 +81,7 @@ return {
     ---@type LazyPluginSpec
     {
         'kndndrj/nvim-dbee',
-        dependencies = {
-            'MunifTanjim/nui.nvim',
-        },
+        dependencies = { 'MunifTanjim/nui.nvim' },
         build = function()
             -- Install tries to automatically detect the install method.
             -- if it fails, try calling it with one of these parameters:
