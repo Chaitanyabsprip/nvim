4cd07907597f62c7cec86eaa79859f18a320bf6f
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2024-10-09 00:57:00 +0530

    feat(rust): add rust to ensure_installed treesitter

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -8,4 +8,11 @@ return {
             vim.g.rustaceanvim = { server = { on_attach = require('lsp').on_attach } }
         end,
     },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        optional = true,
+        opts = function(_, opts)
+            require('config.lazy').extend_opts_list(opts, 'ensure_installed', 'rust')
+        end,
+    },
 }
