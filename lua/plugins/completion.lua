ead5a5cb07dbf69f8f3f4c763f7bd0abfad9278b
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2025-07-10 10:13:19 +0530

    feat: add avante.nvim

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -110,6 +110,22 @@ return {
             }
         end,
     },
+    {
+        'yetone/avante.nvim',
+        build = 'make',
+        event = 'VeryLazy',
+        version = false, -- Never set this value to "*"! Never!
+        enabled = false,
+        dependencies = {
+            'nvim-lua/plenary.nvim',
+            'MunifTanjim/nui.nvim',
+        },
+        ---@module 'avante'
+        ---@type avante.Config
+        opts = {
+            provider = 'copilot',
+        },
+    },
     get_capabilities = function()
         local capabilities = require('lsp').capabilities()
         local ok, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
