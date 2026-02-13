b15065068174f0768534f115b0eb16a92ea8ce17
Chaitanya Sharma <chaitanyasanjeevsharma@gmail.com>
2026-02-14 01:53:31 +0530

    feat: add java support

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -1,12 +1,23 @@
+local configure = require('plugins.lsp').configure
+local function jdtls() configure 'jdtls' end
 ---@module "lazy"
 ---@type LazySpec[]
 return {
     {
         'nvim-java/nvim-java',
         ft = { 'java' },
-        config = function()
-            require('java').setup()
-            vim.lsp.enable 'jdtls'
+        opts = {},
+    },
+    {
+        'neovim/nvim-lspconfig',
+        optional = true,
+        opts = { servers = { jdtls = jdtls } },
+    },
+    {
+        'nvim-treesitter/nvim-treesitter',
+        optional = true,
+        opts = function(_, opts)
+            require('config.lazy').extend_opts_list(opts, 'ensure_installed', 'java')
         end,
     },
 }
